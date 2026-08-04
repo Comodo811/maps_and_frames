5d8d7cdb1d2c87deaa8727512ab01e7a5a59474a
unknown <you@example.comquamshini@gmail.com>
2026-08-05 00:20:26 +0100

    Fix compatibility with StationAPI 2.0.0-alpha.6.2

--- src/main/java/io/github/comodo811/mapsandframes/events/init/EntityListener.java
+++ src/main/java/io/github/comodo811/mapsandframes/events/init/EntityListener.java
@@ -1,16 +1,16 @@
 package io.github.comodo811.mapsandframes.events.init;
 
 import net.mine_diver.unsafeevents.listener.EventListener;
-import net.modificationstation.stationapi.api.event.entity.EntityRegister;
+import net.modificationstation.stationapi.api.event.entity.EntityRegisterEvent;
+import io.github.comodo811.mapsandframes.MapsAndFrames;
 import io.github.comodo811.mapsandframes.entity.ItemFrameEntity;
 
 public class EntityListener {
 
     @EventListener
-    public static void registerEntities(EntityRegister event) {
+    public static void registerEntities(EntityRegisterEvent event) {
 
-        event.register(ItemFrameEntity.class, "mapsandframes:item_frame");
+        event.register( MapsAndFrames.NAMESPACE.id("item_frame"), ItemFrameEntity.class);
 
     }
 }
-
